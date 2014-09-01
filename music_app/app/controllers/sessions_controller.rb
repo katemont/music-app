132d5274be7f6a8c19b04f7b53634d255ad041dd
katemont <katharine.montgomery@gmail.com>
2014-09-01 17:01:46 +0100

    adds bootstrap stylesheets etc

--- music_app/app/controllers/sessions_controller.rb
+++ music_app/app/controllers/sessions_controller.rb
@@ -1,12 +1,5 @@
 class SessionsController < ApplicationController
   def new
-    # if # how do we know if a user is authenticated?
-    #   session[:user_id] = user.id
-    #   redirect_to root_url, notice: "Logged in!"
-    # else
-    #   flash.now.alert = "Invalid email or password"
-    #   render "new"
-    # end
   end
 
  def create
--- music_app/app/models/user.rb
+++ music_app/app/models/user.rb
@@ -6,4 +6,8 @@ class User < ActiveRecord::Base
   validates :email, presence: true
   validates :email, uniqueness: { case_sensitive: false }
 
+  def role?(role_to_compare)
+      self.role.to_s == role_to_compare.to_s
+  end
+
 end
