5e615d158134c281974f899c6f3a667c2aa221d1
katemont <katharine.montgomery@gmail.com>
2014-09-01 16:37:07 +0100

    creates working user login and logout with validations

--- music_app/app/controllers/application_controller.rb
+++ music_app/app/controllers/application_controller.rb
@@ -2,10 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   helper_method :current_user
-  
+
+
   private
     def current_user
-      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+      begin
+          @current_user ||= User.find(session[:user_id]) if session[:user_id]
+      rescue ActiveRecord::RecordNotFound
+        session[:user_id] = nil
+      end
     end
   end
-
--- music_app/app/controllers/sessions_controller.rb
+++ music_app/app/controllers/sessions_controller.rb
@@ -1,12 +1,12 @@
 class SessionsController < ApplicationController
   def new
-    if # how do we know if a user is authenticated?
-      session[:user_id] = user.id
-      redirect_to root_url, notice: "Logged in!"
-    else
-      flash.now.alert = "Invalid email or password"
-      render "new"
-    end
+    # if # how do we know if a user is authenticated?
+    #   session[:user_id] = user.id
+    #   redirect_to root_url, notice: "Logged in!"
+    # else
+    #   flash.now.alert = "Invalid email or password"
+    #   render "new"
+    # end
   end
 
  def create
@@ -25,3 +25,5 @@ class SessionsController < ApplicationController
     redirect_to root_url, notice: "Logged out!"
   end
 end
+
+
