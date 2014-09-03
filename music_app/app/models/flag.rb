9282c4ea724b487a1d4b19061381a5f7528541e3
katemont <katharine.montgomery@gmail.com>
2014-09-03 17:40:50 +0100

    adds more music with artwork

--- music_app/app/models/flag.rb
+++ music_app/app/models/flag.rb
@@ -5,11 +5,5 @@ belongs_to :user
 belongs_to :comment
 
 validates_uniqueness_of :comment_id, scope: :user_id
-# validates_inclusion_of :value, in: [1, -1]
-# validate :ensure_not_author
-
-#   def ensure_not_author
-#     errors.add :user_id, "is the author of the comment" if comment.user_id == user_id
-#   end
 
 end
