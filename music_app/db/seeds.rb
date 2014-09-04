e80c2b1143b7103c1d7435b25afc40dda5d7984c
katemont <katharine.montgomery@gmail.com>
2014-09-05 00:07:26 +0100

    adds in some seed data

--- music_app/db/seeds.rb
+++ music_app/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.delete_all
+
+User.create name: 'Kate', role: admin, email: katharine.montgomery@gmail, profile: "I built this so please be nice about it"
+User.create name: 'KANYE', role: artist, email: kanye@mememe.com, profile: "I AM YEEZUS"
+User.create name: 'Kim Kardashian', role: listener, email: kanye@mememe.com, profile: "I married Yeezus"
+
+Track.delete_all
+
+Track.create name: 'test track'
+Track.create name: 'test track 2'
