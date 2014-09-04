654c2e02bd75e42342541d49ba0568ceb20f46fb
katemont <katharine.montgomery@gmail.com>
2014-09-04 14:29:51 +0100

    Fixing comments is fun

--- music_app/app/controllers/comments_controller.rb
+++ music_app/app/controllers/comments_controller.rb
@@ -1,10 +1,26 @@
 class CommentsController < ApplicationController
   load_and_authorize_resource
+  
+  def edit
+    @track = Track.find(params[:track_id])
+    @comment = Comment.find(params[:id])
+  end
+
   def create
     @track = Track.find(params[:track_id])
     @comment = Comment.build_from(@track, current_user.id, params[:comment][:body] )
     flash[:notice] = "That comment did not save" if !@comment.save
     redirect_to @track
+  end
+
+  def update
+    @track = Track.find(params[:track_id])
+
+    if @comment.update_attributes(params[:comment])
+    redirect_to @track, notice: 'Track successfully updated.'
+    else
+      render action: "edit" 
+    end   
   end
 
   def destroy
@@ -13,8 +29,5 @@ class CommentsController < ApplicationController
     @comment.destroy
     redirect_to @track
   end
-
-  
-
   
 end
