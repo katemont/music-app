e8fcd493bc92c8c49a214c406d8e358734d48165
katemont <katharine.montgomery@gmail.com>
2014-09-05 00:07:10 +0100

    fixes redirect after user creation to log in  to prevent error message from appearing

--- music_app/app/controllers/users_controller.rb
+++ music_app/app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
 
       if @user.save
-         redirect_to @user, notice: 'User was successfully created.'
+         redirect_to "/sessions/new", notice: 'User was successfully created.'
       else
         render action: "new" 
       end
